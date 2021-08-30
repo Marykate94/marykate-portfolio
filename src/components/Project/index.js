a341f8a0c6199618d9160f8596114b43afb0a933
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-08-30 19:25:29 -0400

    getting projects to display images

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,54 +1,57 @@
 import React from "react";
+// import Image from "../../assets/images/falconry-blog.png";
+import projects from "../../project.json";
+// export const projects = [
+//     {
+//       id: 1,
+//       title: "Falconry Blog",
+//       subtitle: "JavaScript, Handlebars, CSS",
+//       description:
+//         "A blog about Falconry",
+//       image: "../../assets/images/falconry-blog.png",
+//       link: "https://falconry-blog.herokuapp.com/",
+//     },
+//     {
+//       id: 2,
+//       title: "Transley Notes",
+//       subtitle: "JavaScript, HTML and CSS",
+//       description:
+//         "Note taker application",
+//       image: "../images/transley-notes.png",
+//       link: "https://transley-notes.herokuapp.com",
+//     },
+//   ];
 
-export const projects = [
-    {
-      title: "Falconry Blog",
-      subtitle: "JavaScript, Handlebars, CSS",
-      description:
-        "A blog about Falconry",
-      image: "../images/falconry-blog.png",
-      link: "https://falconry-blog.herokuapp.com/",
-    },
-    {
-      title: "Transley Notes",
-      subtitle: "JavaScript, HTML and CSS",
-      description:
-        "Note taker application",
-      image: "../images/transley-notes.png",
-      link: "https://transley-notes.herokuapp.com",
-    },
-  ];
-
-export default function Project() {
-  return (
-    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-        <div className="flex flex-wrap -m-4">
-          {projects.map((project) => (
-            <a
-              href={project.link}
-              key={project.image}
-              className="sm:w-1/2 w-100 p-4">
-              <div className="flex relative">
-                <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={project.image}
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
-                  </h1>
-                  <p className="leading-relaxed">{project.description}</p>
+  export default function Project() {
+    return (
+      <section id="projects">
+        <div>
+          <div>
+            {projects.map((project) => (
+              <a
+                href={project.link}
+                key={project.id}
+                >
+                <div>
+                  <img
+                    alt="project-images"
+        
+                    src={project.image}
+                  />
+                  <div>
+                    <h2>
+                      {project.subtitle}
+                    </h2>
+                    <h1>
+                      {project.title}
+                    </h1>
+                    <p>{project.github}</p>
+                  </div>
                 </div>
-              </div>
-            </a>
-          ))}
+              </a>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
-  );
-}
+      </section>
+    );
+  }
