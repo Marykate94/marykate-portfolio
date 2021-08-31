2bf650d9f4e78d3db55e9bae65a8c482ab103d1a
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-08-30 21:16:06 -0400

    added resume document

---
+++ src/components/Resume/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Resume() {
+  return (
+    <section class="container">
+      <h2 className="top-title">Resume</h2>
+      <hr></hr>
+
+      <div>
+        <div class="mt-5">
+          <h2 className="top-title">Marykate Smith</h2>
+          <ul>
+            <li>
+              Junior full stack web developer.
+            </li>
+          </ul>
+
+          <a
+            href=""
+            class="link"
+          >
+            View my Resume
+          </a>
+        </div>
+      </div>
+
+    </section>
+  );
+}
+
+export default Resume;
