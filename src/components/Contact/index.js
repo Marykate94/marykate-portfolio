d61f9e6aaf48537325b4f430ae4083f2460f597c
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-09-03 12:11:44 -0400

    added styling to contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import { validateEmail } from '../../utils/helpers';
+import './style.css';
 
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
@@ -37,18 +38,18 @@ function ContactForm() {
   };
 
   return (
-    <section>
+    <section class="content">
       <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
+        <div class="info">
           <label htmlFor="name">Name:</label>
           <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
-        <div>
+        <div class="info">
           <label htmlFor="email">Email address:</label>
           <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
-        <div>
+        <div class="info">
           <label htmlFor="message">Message:</label>
           <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
         </div>
